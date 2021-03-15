6d4950fc04c53e6a5fd0155e3f1f992f3eb52ddc
Volodymyr Udovychenko <vosap2007@i.ua>
2021-03-15 23:27:13 +0200

    добавляет Suspense, lazy

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Switch } from 'react-router';
 import { connect } from 'react-redux';
 import AppBar from './AppBar';
-import HomeViev from '../views/HomeViev';
-import LoginViev from '../views/LoginViev';
-import RegisterViev from '../views/RegisterViev';
-import ContactsViev from '../views/ContactsViev';
 import authOperations from "../redux/auth/auth-operations";
 import PrivateRoute from "./PrivateRoute";
 import PublicRoute from "./PublicRoute";
 //import {Button} from "react-bootstrap";
+
+const HomeViev = lazy(() => import('../views/HomeViev'));
+const LoginViev = lazy(() => import('../views/LoginViev'));
+const RegisterViev = lazy(() => import('../views/RegisterViev'));
+const ContactsViev = lazy(() => import('../views/ContactsViev'));
 
 
 class App extends Component {
@@ -25,13 +26,14 @@ class App extends Component {
 
         <div>
           <AppBar />
-
+<Suspense fallback={<p>Загружаем...</p>}>
           <Switch>
             <PublicRoute exact path="/" component={HomeViev} />
             <PublicRoute path="/register" redirectTo = '/contacts' restricted component={RegisterViev} />
             <PublicRoute path="/login" redirectTo = '/contacts' restricted component={LoginViev} />
             <PrivateRoute path="/contacts" redirectTo = '/login' component={ContactsViev} />
           </Switch>
+          </Suspense>
         </div>
       </>
     );
